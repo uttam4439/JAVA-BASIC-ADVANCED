11c08976ac82e23009db1cf114a8608804983c74
UTTAM KUMAR <rajuttam4439@gmail.com>
2025-09-18 23:24:53 +0530

    Cyclic Graph

---
+++ Graph/CyclicGraph.java
@@ -0,0 +1,55 @@
+package Graph;
+import java.util.*;
+
+public class CyclicGraph{
+    static class DirectedEdge{
+        int src;
+        int dest;
+
+        public DirectedEdge(int s, int d){
+            this.src = s;
+            this.dest = d;
+        }
+    }
+
+    public static void directedGraph(ArrayList<DirectedEdge> graph[]){
+        for(int i =0; i<graph.length; i++){
+            graph[i] = new ArrayList<DirectedEdge>();
+        }
+        graph[0].add(new DirectedEdge(0, 2));
+        graph[1].add(new DirectedEdge(1,0));
+        graph[2].add(new DirectedEdge(2,3));
+        graph[3].add(new DirectedEdge(3,0));
+    }
+
+    public static boolean isCyclic(ArrayList<DirectedEdge> graph[], boolean[] vis, int curr, boolean[] rec){
+        // boolean rec[] array track krne ke liye h curr. element ke neighbour ki cyclic h ya nhi
+
+        vis[curr] = true;
+        rec[curr] = true;
+
+        for(int i =0; i<graph[curr].size(); i++){
+            DirectedEdge e = graph[curr].get(i);
+
+            if(rec[e.dest] == true){
+                return true;
+            }else if(vis[e.dest] == false){
+                if(isCyclic(graph, vis, e.dest, rec) == true){
+                    return true;
+                }
+            }
+        }
+        rec[curr] = false;
+        return false;
+    }
+    public static void main(String[] args){
+        int V = 4;
+
+        //yanha array create kr rahe wo bhi ArrayList datatype ka na ki arraylist
+        ArrayList<DirectedEdge> graph[] = new ArrayList[V]; 
+        directedGraph(graph);
+
+
+        System.out.println(isCyclic(graph, new boolean[V], 0, new boolean[V]));
+    }
+}
--- Graph/RottingOranges.java
+++ Graph/RottingOranges.java
@@ -1,9 +1,5 @@
 package Graph;
 public class RottingOranges{
 
-    static class allEdges{
-        int row;
-        int col;
-        int src
-    }
+    int[][] = {}
 }
