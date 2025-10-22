1f7b29d759907bf0d69cba0ef30a679d4e08edf0
UTTAM KUMAR <rajuttam4439@gmail.com>
2025-10-23 01:35:21 +0530

    Josephus Problem

---
+++ TreeDSA/Josephusproblem.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Josephusproblem{
+    public static void main(String[] args){
+
+        ArrayList<Integer> gg = new ArrayList<>(40);
+        int n = 40;
+        int k = 7;
+
+        for(int i = 1; i<=n; i++){
+            gg.add(i);
+        }
+        int ans = survived(gg, 0, k-1);
+
+        System.out.println(ans);
+
+    }
+
+    public static int survived(ArrayList<Integer> gg , int index , int k ){
+        if(gg.size() == 1){
+            return gg.get(0);
+            
+        }
+
+        index = (index + k) % gg.size();
+        gg.remove(index);
+
+        return survived(gg, index, k);
+    }
+}
---
+++ TreeDSA/class BalancedParentheses{.java
@@ -0,0 +1,48 @@
+import java.util.*;
+class BalancedParentheses{
+    public static void main(String[] args){
+        ArrayList<String> gg = new ArrayList<>();
+        StringBuilder output = new StringBuilder(" ");
+        int n = 3;
+        solver(n,n,output,gg);
+        System.out.println(gg);
+
+        ArrayList<String> kk = new ArrayList<>();
+        solver(n, n, " ", kk);
+        System.out.println(kk);
+    }
+
+    public static void solver(int open , int close , StringBuilder output , ArrayList<String> gg){
+        if(open == 0 && close == 0){
+            gg.add(output.toString());
+            return;
+        }
+        if(open != 0){
+            output.append("(");
+            solver(open-1,close,output,gg);
+            output.deleteCharAt(output.length()-1);
+        }
+        if(open < close){
+            output.append(")");
+            solver(open , close-1, output, gg);
+            output.deleteCharAt(output.length()-1);
+        }
+    }
+
+    public static void solver(int open , int close , String output , ArrayList<String> kk){
+        if(open == 0 && close == 0){
+            kk.add(output);
+            return;
+        }
+        if(open != 0){
+            
+            solver(open-1,close,output+"(",kk);
+
+        }
+        if(open < close){
+            
+            solver(open , close-1, output+")", kk);
+
+        }
+    }
+}
